576b5ea642714c4d384d3ba62c4f0877e691af21
Roman Koshovenko <kdrk.ru@gmail.com>
2026-09-18 20:01:21 +0700

    Configure GitHub Pages automated deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: isGithubActions ? 'export' : undefined,
+  basePath: isGithubActions ? '/cenlaro-site' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
